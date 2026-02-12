2a92b8af7ab52d2dbca1d70ade7962c9bcaaaed4
Yasser Hegazy <yasseranahegazy@gmail.com>
2026-02-12 16:49:51 +0200

    Add tests for the empty files, and the files with only new lines

--- tests/test_file_counter.py
+++ tests/test_file_counter.py
@@ -2,3 +2,9 @@ from file_counter import file_counter
 
 def test_file_counter_valid_file():
     assert 5 == file_counter.count_lines("testdata/file_with_5_lines.txt")
+    
+def test_file_counter_empty_file():
+    assert 0 == file_counter.count_lines("testdata/empty_file.txt")
+
+def test_file_counter_file_with_only_newlines():
+    assert 3 == file_counter.count_lines("testdata/file_with_only_newlines.txt")
