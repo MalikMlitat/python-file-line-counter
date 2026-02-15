8570244e20698bf795ee8bdf40d953c819e37fea
Mohammed <Mohammedoghanem17@gmail.com>
2026-02-15 10:40:38 +0200

    Add tests for count_lines

--- tests/test_file_counter.py
+++ tests/test_file_counter.py
@@ -1,4 +1,23 @@
+import pytest
 from file_counter import file_counter
 
-def test_file_counter_valid_file():
-    assert 5 == file_counter.count_lines("testdata/file_with_5_lines.txt")
+
+def test_file_counter_valid_file(tmp_path):
+
+    test_file = tmp_path / "file_with_5_lines.txt"
+    test_file.write_text("Line1\nLine2\nLine3\nLine4\nLine5\n")
+    
+   
+    assert file_counter.count_lines(str(test_file)) == 5
+
+#Empty file
+def test_file_counter_empty_file(tmp_path):
+    empty_file = tmp_path / "empty.txt"
+    empty_file.write_text("")
+    assert file_counter.count_lines(str(empty_file)) == 0
+
+# File not found
+def test_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        file_counter.count_lines("not_existing_file.txt")
+
